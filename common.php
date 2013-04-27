9e6da08353ee6a779b55ee56385123af6b32538e
hexa- <mweinelt@gmail.com>
2013-04-27 08:41:08 +0200

    fix header css; url must be relative to css directory, not document root

--- common.php
+++ common.php
@@ -5,7 +5,7 @@ require_once($rootpath.'config.php');
 $tpl = new tpl;
 $tpl->assign_vars("BOOTSRAPPATH", $rootpath.'bootstrap/');
 $tpl->assign_vars("PROJEKTNAME", PROJEKTNAME);
-$tpl->add_css_file("./css/common.css");
+$tpl->add_css_file($rootpath."css/common.css");
 
 require_once($rootpath.'lib/defines.php');
 ?>
