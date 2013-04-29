d54739df6ce5ac82df39293416b3880b7fe3783f
Doomsta <andrerandel2k7@googlemail.com>
2013-04-30 00:07:32 +0200

    add shoutbox config.dist

---
+++ shout_box/sb_files/data/config.dist.php
@@ -0,0 +1,21 @@
+<?php
+$adminPass = "admin";
+$title = "Sh";
+$entriesPerPage = 5;
+$badWords = "";
+$width = 200;
+$color_background = "#808285";
+$color_background_center = "#FFFFFF";
+$color_cell_left = "#73C6EF";
+$color_cell_right = "#B5CEDE";
+$font_color = "#000000";
+$font_family = "Arial";
+$font_size = 12;
+$link_color = "#000000";
+$link_color_hover = "#EF6329";
+$link_style = "underline";
+$link_style_hover = "underline";
+$border_width = 1;
+$border_style = "solid";
+$border_color = "#000000";
+?>
