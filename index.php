38a759c9a9984ccaed9d7e5522b1a9e4ee3f75ab
Doomsta <andrerandel2k7@googlemail.com>
2013-05-04 07:12:18 +0200

    add some content to index.php

--- index.php
+++ index.php
@@ -1,8 +1,21 @@
 <?php
 include ('common.php');
+$tables = array('global', 'by_class', 'by_character', 'by_battleground');
+foreach($tables as $tableName)
+{ 
+	$query = 'SELECT count(*) as value FROM '.$tableName;
+	$result = mysql_query($query);
+	$anzahl = mysql_fetch_object($result);
+	$out[$tableName] = number_format($anzahl->value, 0, ',', '.'); 
+}
 
+$query = 'SELECT timestamp FROM global';
+$result = mysql_query($query);
+$row = mysql_fetch_object($result);
+$first = date('H:i d.m.y', $row->timestamp);
 
-
+$tpl->assign_vars('FIRST', $first);
+$tpl->assign_vars('TABLEDATA', $out);
 $tpl->add_js_file("./lib/Chart.js");
 $tpl->set_vars(array(
 			'page_title'		=> 'PvP@Castle Home',
